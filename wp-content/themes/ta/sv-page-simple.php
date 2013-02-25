35c6d9a19b4c28b6437c783187bd1ed62474e7fc
Frank Hebbert <frank@hebbert.com>
2013-02-25 12:08:05 -0500

    added google analytics snippet

--- wp-content/themes/ta/sv-page-simple.php
+++ wp-content/themes/ta/sv-page-simple.php
@@ -139,5 +139,24 @@
                }
              </script>
              
+             <script type="text/javascript">
+             var _gaq = _gaq || [];
+             _gaq.push(["_setAccount", "UA-10594404-1"]);
+             _gaq.push(['_setCampSourceKey', 'utm_source']);
+             _gaq.push(['_setCampMediumKey', 'utm_medium']);
+             _gaq.push(['_setCampContentKey', 'utm_content']);
+             _gaq.push(['_setCampTermKey', 'utm_term']);
+             _gaq.push(['_setCampNameKey', 'utm_campaign']);
+             _gaq.push(['_setDomainName', '.transalt.org']);
+             _gaq.push(['_setAllowHash', false]);
+             _gaq.push(['_setAllowLinker', true]);
+             _gaq.push(["_trackPageview"]);
+             (function() {var ga = document.createElement("script");ga.type =
+             "text/javascript";ga.async = true;ga.src = ("https:" ==
+             document.location.protocol ? "https://ssl" : "http://www") +
+             ".google-analytics.com/ga.js";var s =
+             document.getElementsByTagName("script")[0];s.parentNode.insertBefore(ga,
+             s);})();
+             </script>
     </body>
 </html>
