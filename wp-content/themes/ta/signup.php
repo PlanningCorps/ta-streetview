8b611eded1eb8f8c89291d39ccf0334c9005827e
Frank Hebbert <frank@hebbert.com>
2013-03-08 08:02:32 -0500

    capturing emails

---
+++ wp-content/themes/ta/signup.php
@@ -0,0 +1,19 @@
+<?php
+
+//include "konnect.php";
+
+// check if we got a posted value
+
+if (isset($_GET[email])) {
+
+  // clean it
+  $email = mysql_real_escape_string($_GET[email]);
+  
+  mail('hebbert@gmail.com', '56forward contact', $email);
+
+  // store it
+  //storeemail($email);
+
+}
+
+?>
